f5689abfe756577a8f572d9d2512f824e97ec9ce
Danny Kopping <dannykopping@gmail.com>
2014-03-23 13:05:41 +0200

    Completed simple example

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -7,18 +7,36 @@ $(function () {
     $('button.json').click(submitJSON);
 });
 
+var displayInfo = function (response) {
+    var data = {};
+    $.each(response, function (i, element) {
+        data[element.name] = element.value;
+    });
+
+    var infoBox = $('#infoBox');
+    var text = data.firstName + ' ' + data.lastName + ' has signed up with an email address of ' + data.emailAddress
+        + '. ' + (data.gender == 'male' ? 'He' : 'She') + ' has '
+        + (data.spamMe ? '' : 'not') + ' agreed to be spammed.';
+
+    infoBox.text(text);
+    infoBox.show();
+};
+
 /**
  * Submit form as JSON
  */
 var submitJSON = function () {
+    $('#infoBox').hide();
+    $('#infoBox').text('');
+
     $.ajax({
         data: JSON.stringify($('#registrationForm').serializeArray()),
         url: '/server/json.php',
         dataType: 'json',
         contentType: 'text/json',
         type: 'POST',
-        success: function () {
-            console.log(this);
+        success: function (responseData) {
+            displayInfo(responseData);
         }
     });
 };
@@ -27,6 +45,9 @@ var submitJSON = function () {
  * Submit form as AMF
  */
 var submitAMF = function () {
+    $('#infoBox').hide();
+    $('#infoBox').text('');
+
     // send as stream of bytes
     var xhrSend = XMLHttpRequest.prototype.send;
     XMLHttpRequest.prototype.send = function (data) {
@@ -45,8 +66,8 @@ var submitAMF = function () {
         type: 'POST',
         contentType: 'application/x-amf',
         mimeType: 'application/x-amf; charset:x-user-defined',
-        success: function () {
-            console.log(this);
+        success: function (responseData) {
+            displayInfo(AMF.parse(responseData));
         }
     });
 
